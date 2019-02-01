17ab46e6ac9f3b74263cfa2a1ff823828ee93b9a
Henrik Andersen <henr563a@edu.easj.dk>
2019-02-01 13:32:53 +0100

    Done

--- KaffeConsoleAppNetCore/Program.cs
+++ KaffeConsoleAppNetCore/Program.cs
@@ -3,16 +3,21 @@ using System;
 
 namespace KaffeConsoleAppNetCore
 {
+    using System.Collections.Generic;
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            List<Kaffe> kaffeList = new List<Kaffe>() {new Cortado(), new FlatWhite(), new Latte(), new SortKaffe()};
+            foreach (var kaffe in kaffeList)
+            {
+                Console.WriteLine($"Navn: {kaffe.ToString()}    Styrke: {kaffe.Styrke()}    Pris: {kaffe.Pris()}");
+            }
 
-            
 
-           
-
+            Console.ReadLine();
         }
     }
 }
